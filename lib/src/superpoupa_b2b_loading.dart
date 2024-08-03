7f101c39cd288545b74e36d192631b464f5eee48
jsouza.1650 <jsouza.1650@gmail.com>
2024-08-03 13:58:54 +0000

    feat: created SuperPoupaB2BLoading

---
+++ lib/src/superpoupa_b2b_loading.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SuperPoupaB2BLoading extends StatelessWidget {
+  const SuperPoupaB2BLoading({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
